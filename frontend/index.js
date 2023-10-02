f5728ce448fff94c0955d4b6ee7382f563e3c5bb
Sean Kipinä <paintballsean@gmail.com>
2023-10-02 16:43:11 +0300

    Added mentors response api.

--- frontend/index.js
+++ frontend/index.js
@@ -4,11 +4,14 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
-
-
+  
+  // Fetch learner and mentor data from the API
   const learnersResponse = await fetch('http://localhost:3003/api/learners');
   const learnersData = await learnersResponse.json();
-  console.log(learnersData)
+
+  const mentorsResponse = await fetch('http://localhost:3003/api/mentors');
+  const mentorsData = await mentorsResponse.json();
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
