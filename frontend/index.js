ef43e7b11745468c735dfc5e46fd9425fd402453
Sean Kipinä <paintballsean@gmail.com>
2023-10-02 17:12:58 +0300

    Added card container and build leearner card function.

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,33 @@
-async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
+async function sprintChallenge5() {
+  // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
-  const footer = document.querySelector('footer')
-  const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
-  
+  const footer = document.querySelector("footer");
+  const currentYear = new Date().getFullYear();
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`;
+
   // Fetch learner and mentor data from the API
-  const learnersResponse = await fetch('http://localhost:3003/api/learners');
+  const learnersResponse = await fetch("http://localhost:3003/api/learners");
   const learnersData = await learnersResponse.json();
 
-  const mentorsResponse = await fetch('http://localhost:3003/api/mentors');
+  const mentorsResponse = await fetch("http://localhost:3003/api/mentors");
   const mentorsData = await mentorsResponse.json();
 
+  // Get the cards container and initialize info text
+  const cardsContainer = document.querySelector('.cards');
+  const infoParagraph = document.querySelector('.info');
+  infoParagraph.textContent = 'No learner selected';
+
+    // Build learner cards
+    learnersData.forEach((learner) => {
+      const learnerCard = buildLearnerCard(learner, mentorsData);
+      cardsContainer.appendChild(learnerCard);
+    });
+  }
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
-else sprintChallenge5()
+if (typeof module !== "undefined" && module.exports)
+  module.exports = { sprintChallenge5 };
+else sprintChallenge5();
