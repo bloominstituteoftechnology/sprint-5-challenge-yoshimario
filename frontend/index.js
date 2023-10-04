b6d83dc979e7a795352ee604923e68221b7ed10f
Sean Kipinä <paintballsean@gmail.com>
2023-10-04 21:17:50 +0300

    Got other test cases to pass except 17,20,22.

--- frontend/index.js
+++ frontend/index.js
@@ -13,65 +13,71 @@ async function sprintChallenge5() {
       throw new Error("Failed to fetch learner data");
     }
     const learnersData = await learnersResponse.json();
-
+  
     // Fetch mentor data
     const mentorsResponse = await fetch("http://localhost:3003/api/mentors");
     if (!mentorsResponse.ok) {
       throw new Error("Failed to fetch mentor data");
     }
     const mentorsData = await mentorsResponse.json();
-
+  
     const cards = document.querySelector(".cards");
     const selectedLearnerInfo = document.querySelector(".info");
     selectedLearnerInfo.textContent = "No learner is selected";
-
+  
     learnersData.forEach((learner) => {
       const learnerCard = buildLearnerCard(learner, mentorsData);
       cards.appendChild(learnerCard);
-
+  
       learnerCard.addEventListener("click", () => {
         const isSelected = learnerCard.classList.contains("selected");
-
+      
         if (!isSelected) {
           // Deselect all cards first
           document.querySelectorAll(".card").forEach((card) => {
             card.classList.remove("selected");
+            const learnerIdElement = card.querySelector("h3");
+            if (learnerIdElement) {
+              learnerIdElement.textContent = learner.fullName; // Remove the ID
+            }
           });
           // Select the clicked card
           learnerCard.classList.add("selected");
-          selectedLearnerInfo.textContent = `The selected learner is ${learner.fullName}`;
-          // Check if the learner ID element exists
           const learnerIdElement = learnerCard.querySelector("h3");
           if (learnerIdElement) {
             learnerIdElement.textContent = `${learner.fullName}, ID: ${learner.id}`;
           }
+          // Update selectedLearnerInfo with the selected learner's name
+          selectedLearnerInfo.textContent = `The selected learner is ${learner.fullName}`;
         } else {
           // Deselect the clicked card
           learnerCard.classList.remove("selected");
-          selectedLearnerInfo.textContent = "No learner is selected";
-          // Check if the learner ID element exists
           const learnerIdElement = learnerCard.querySelector("h3");
           if (learnerIdElement) {
-            learnerIdElement.textContent = learner.fullName;
+            learnerIdElement.textContent = learner.fullName; // Remove the ID
           }
+          // Update selectedLearnerInfo when no learner is selected
+          selectedLearnerInfo.textContent = "No learner is selected";
         }
       });
+      
+      
     });
-
+  
     function buildLearnerCard(learner, mentorsData) {
       const card = document.createElement("div");
       card.classList.add("card");
-
+  
       const learnerNameH3 = document.createElement("h3");
       learnerNameH3.textContent = learner.fullName;
-
+  
       const emailDiv = document.createElement("div");
       emailDiv.textContent = learner.email;
-
+  
       const mentorNameH4 = document.createElement("h4");
       mentorNameH4.textContent = "Mentors";
       mentorNameH4.classList.add("closed");
-
+  
       const mentorListUl = document.createElement("ul");
       learner.mentors.forEach((mentorName) => {
         mentorsData.forEach((mentor) => {
@@ -83,23 +89,24 @@ async function sprintChallenge5() {
           }
         });
       });
-
+  
       [learnerNameH3, emailDiv, mentorNameH4, mentorListUl].forEach(
         (element) => {
           card.appendChild(element);
         }
       );
-
+  
       mentorNameH4.addEventListener("click", () => {
         mentorNameH4.classList.toggle("open");
         mentorNameH4.classList.toggle("closed");
       });
-
+  
       return card;
     }
   } catch (error) {
     console.error("An error occurred:", error);
   }
+  
 
   // 👆 WORK ABOVE THIS LINE 👆
 }
